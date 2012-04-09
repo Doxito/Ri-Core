8dd2fbd3721887301395b22373ba83213a910e5a
kaelima <kaelima@live.se>
2012-04-09 14:28:46 +0200

    Core/Scripts: Remove some unnecessary creature/player casts in script

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -803,11 +803,11 @@ public:
             {
                 for (std::list<Creature*>::const_iterator itr = MinionList.begin(); itr != MinionList.end(); ++itr)
                 {
-                    if (CAST_CRE(*itr)->GetOwner()->GetGUID() == me->GetOwner()->GetGUID())
+                    if ((*itr)->GetOwner()->GetGUID() == me->GetOwner()->GetGUID())
                     {
-                        if (CAST_CRE(*itr)->isInCombat() && CAST_CRE(*itr)->getAttackerForHelper())
+                        if ((*itr)->isInCombat() && (*itr)->getAttackerForHelper())
                         {
-                            AttackStart(CAST_CRE(*itr)->getAttackerForHelper());
+                            AttackStart((*itr)->getAttackerForHelper());
                         }
                     }
                 }
@@ -820,13 +820,12 @@ public:
             {
                 if (Unit* owner = me->GetOwner())
                 {
-                    if (owner->GetTypeId() == TYPEID_PLAYER && CAST_PLR(owner)->isInCombat())
+                    Player* plrOwner = owner->ToPlayer();
+                    if (plrOwner && plrOwner->isInCombat())
                     {
-                        if (CAST_PLR(owner)->getAttackerForHelper() && CAST_PLR(owner)->getAttackerForHelper()->GetEntry() == GHOSTS)
-                        {
-                            AttackStart(CAST_PLR(owner)->getAttackerForHelper());
-                        }
-                        else
+                        if (plrOwner->getAttackerForHelper() && plrOwner->getAttackerForHelper()->GetEntry() == GHOSTS)
+                            AttackStart(plrOwner->getAttackerForHelper());
+                             else
                         {
                             FindMinions(owner);
                         }
